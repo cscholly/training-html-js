4a8b4c7ba76caf01dd42f0c4afc51fa59b6cfcb9
cscholly <caspers@intelliacc.com>
2014-11-15 10:11:35 +0200

    get amount and calc vat in js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert("We're working on it...");
+// declare variables
+var exvat = 0;
+var inclvat = 0;
+var vatamount = 0;
+var totalamount = 0;
+var vatpercentage = 1.14;
+
+// calculation
+totalamount = exvat * vatpercentage;
+
+alert(totalamount);
