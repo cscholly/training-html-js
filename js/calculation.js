99b786c4b05d644ee4a1e3dd8e6825349cadfd37
cscholly <caspers@intelliacc.com>
2015-06-14 20:22:45 +0200

    Reformatted to Bootstrap and redone JS

--- js/calculation.js
+++ js/calculation.js
@@ -8,6 +8,10 @@ var vatpercentage = 1.14;
 //get value from user and calc VAT
 function calcVAT() { 
 var exvat = document.getElementById('input').value; 
-totalamount = Math.round((exvat * vatpercentage)*100)/100;
-document.getElementById('result').value = totalamount; 
+exvat = parseFloat(exvat).toFixed(2);
+vatamount = parseFloat(((exvat * 14)/100)).toFixed(2);
+totalamount = parseFloat(Math.round((exvat * vatpercentage)*100)/100).toFixed(2);
+document.getElementById('exclude_vat').innerHTML = exvat;
+document.getElementById('vat').innerHTML = vatamount;
+document.getElementById('include_vat').innerHTML = totalamount;
 }
