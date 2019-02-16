0b6448fcab641a994ad9babfad8a02b000c81abd
Casper Scholly <caspers@scholly.co.za>
2019-02-16 20:55:41 +0200

    text added to policy

--- privacy-policy.php
+++ privacy-policy.php
@@ -25,18 +25,123 @@
 
     <div id=wrap>
         <div class="container">
-            <!-- News and Updates pop-up start-->
-            <div class="alert alert-info alert-dismissible fade in" role="alert" style="margin-top:30px" ;>
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <strong>Update: This website is now secure (SSL)</strong>
-            </div>
-            <!-- News and Updates pop-up end -->
             <div class="page-header">
                 <h1>Helpful information <small>What is South African VAT all about?</small></h1>
             </div>
             <div class="row">
                 <div class="col-md-12">
-                  <p>hello world</p>
+                  <p>Persons accessing the VAT Calculator web site, subject themselves to and agree to VAT Calculator terms conditions and privacy policy, as set out below.
+
+WEBSITE DISCLAIMER
+The information and content made available on this Web Site is provided by VAT Calculator, detailing information about our company and its services. We make no representations or warranties, implied or otherwise, that, amongst others, the content available on this Web Site is free from errors or omissions or that the service will be 100% uninterrupted and error free.
+
+We have taken, and will continue to take, due care and diligence that all information provided on this Web Site is, to the best of our knowledge and understanding, true and correct. However, VAT Calculator shall not be liable for any damage, loss or liability of whatsoever nature arising from the use or inability to use this Web Site, any information or content provided from and through this Web Site.
+
+There is no warranty of any kind, express or implied, regarding the information supplied on this Web Site or in respect of any aspect of our services. Any warranty implied by law is hereby excluded, except to the extent to which such exclusion would be unlawful.
+
+Information, ideas and opinions expressed on this site should not be regarded as professional advice. Users are encouraged to consult VAT Calculator before taking any course of action related to information or opinions expressed on this site.
+
+This Web Site is supplied on an "as is" basis and has not been compiled or supplied to meet the user's individual requirements. It is the sole responsibility of the user, prior to entering into any agreement with VAT Calculator, to satisfy himself or herself that the service available through this Web Site will meet the user's individual requirements.
+
+USAGE
+This Web Site is only intended to provide the user with information regarding VAT Calculator, products offered and services rendered. VAT Calculator authorises the user to view, copy, download to a local drive, print, and distribute the content of this Web Site, or any part thereof, provided that: a. such content is used for informational and/or non-commercial purposes only; and b. any reproduction of the content of this Web Site, or portions thereof, must include the following copyright notice: © VAT Calculator. Users wishing to use any content from this site for commercial purposes may only do so with prior written permission from VAT Calculator.
+
+INTELLECTUAL PROPERTY RIGHTS
+All content, trademarks and data on this Web Site, including but not limited to text, designs, graphics, and icons are the property of VAT Calculator. As such they are protected from infringement by domestic and international legislation and treaties. Subject to the rights afforded to the user herein, all other rights to all intellectual property on this site are expressly reserved and no license to VAT Calculator intellectual property has been granted on this Web Site.
+
+INFORMATION NOT UPDATED OR TO BE RELIED ON
+While VAT Calculator uses all reasonable efforts to include accurate and up-to-date information on the Web Site, VAT Calculator makes no warranties or representations with respect to the content of the Web Site VAT Calculator assumes no responsibility for updating the Web Site or its content or notifying users of information that is inaccurate, incomplete, or out-of-date.
+
+NO LIABILITY
+VAT Calculator assumes no responsibility, and shall not be liable for, any damages to, or viruses that may infect, your computer equipment or other property on account of your access to, use of, or browsing this Web Site or your downloading of any materials, data, text, or images from the Web Site.
+
+VAT Calculator will not be held responsible or liable for any interruption or discontinuance of any or all functionality of this Web Site, whether the result of actions or omission of VAT Calculator or a third party.
+
+VAT Calculator accepts no responsibility or liability whatsoever arising from or in any way connected with the use of this Web Site or its content. VAT Calculator will not be liable for the accuracy, completeness, adequacy, timeliness or comprehensiveness of the information contained on the Web Site.
+
+CHANGES AND AMENDMENTS
+VAT Calculator expressly reserves the right to alter and/or amend any information set out in this Web Site without notice. Furthermore, VAT Calculator may at any time revise or update these terms and conditions. The user is bound by such revisions and should therefore periodically visit this page.
+
+AGREEMENTS IN TERMS OF SECTION 21 OF THE ELECTRONIC COMMUNICATIONS AND TRANSACTION ACT
+No information or data on this Web Site constitutes a solicitation, recommendation, endorsement or offer by VAT Calculator, but merely an invitation to do business. No agreements shall be concluded merely by sending a data message to this Web Site or its owners. Valid agreements require an acknowledgement and offer, or receipt from this Web Site.
+
+ELECTRONIC COMMUNICATIONS
+By using this Web Site or communicating with VAT Calculator by electronic means, the user consents and acknowledges that any and all agreements, notices, disclosures, or any other communication satisfies any legal requirement, including but not limited to the requirement that such communications should be in writing.
+
+LINKS TO THIRD PARTY SITES
+VAT Calculator provides links to third party sites to the user only as a convenience. Such third party information is independent from that provided by VAT Calculator. VAT Calculator accepts no responsibility for the content or accuracy or the use of such other web sites and does not endorse, and shall not be deemed to have endorsed or accepted, their contents. Linked web sites or pages are not subject to VAT Calculator control. VAT Calculator shall not be held responsible or liable, directly or indirectly, in any way for the contents of such web sites, the use of such web sites, or inability to use or access any linked web sites or any links contained in a linked Web Site.
+
+SECURITY OF INFORMATION
+It is expressly prohibited for any person, business or entity to gain or attempt to gain unauthorised access to any page on this Web Site, or to deliver or attempt to deliver any unauthorised, damaging or malicious code to this Web Site Any person who delivers or attempts to deliver any unauthorised, damaging or malicious code to this Web Site or attempts to gain unauthorised access to any page on this Web Site shall be held criminally liable. In the event that VAT Calculator should suffer any damage or loss, civil damages will be claimed.
+
+SEVERABILITY
+These terms and conditions of use constitute the entire agreement between VAT Calculator and the user of this Web Site Any failure by VAT Calculator to exercise or enforce any right or provision of these terms and conditions of use shall in no way constitute a waiver of such right or provision.
+
+In the event that any term or condition of the use of this Web Site is not fully enforceable or valid for any reason, such term(s) or condition(s) shall be severable from the remaining terms and conditions. The remaining terms and conditions shall not be affected by such enforceability or invalidity and shall remain enforceable and applicable.
+
+APPLICABLE AND GOVERNING LAW
+This Web Site is hosted, controlled and operated from the Republic of South Africa , and thus South African law governs the use or inability to use this Web Site and these terms and conditions.
+
+CONTACT INFORMATION
+Any questions, queries, or requests to use any part of this Web Site can be directed to:
+
+VAT Calculator: Contact Number: 011 915 5023
+Alternatively, please make use of our Contact Us page.
+
+PRIVACY POLICY
+VAT Calculator respects your privacy and is committed to keeping your information confidential. Generally, you can visit our Web Site without having to divulge any personal information about yourself. However, while using this Web Site you may provide information about yourself or it may be collected by us.
+
+Any and all information collected at this site will be kept strictly confidential. It will not be sold, loaned, reused or otherwise disclosed to any organisation. This applies to e-mail addresses and all other personal information.
+
+We will only disclose or report personal information if and when required to do so by law or any regulatory authority and to our employees who require such information to carry out their duties.
+
+Personal information is used as is appropriate in the normal course of business to provide the products and services you have requested. We may retain any information for purposes of ongoing business relationships or to communicate directly with you.
+
+Upon request we will furnish you with details of the personal information we hold about you. You may submit your request via our contact us page. Should you believe that any information we hold about you is incorrect, please inform us at the above page and we will correct it.
+
+Please note that, while all attempts are made to secure information transmitted to this Web Site, if you are submitting personal details or other information over the Internet which you wish to remain private, there is a possibility that information you submit could be observed by a third party while in transit.
+
+E-MAIL ADDRESS
+We will only record your e-mail address if you send us a message. It will only be used for the purpose for which you provide it and will not be added to a mailing list unless you request that this be done. We will not disclose it without your consent.
+
+E-MAIL SUBSCRIPTIONS
+We maintain a list of e-mail addresses to which VAT Calculator Newsletter, our regular newsletter, is sent. Individuals must affirmatively request to join this list by using the subscribe form on the subscriptions page. Any members of this list may choose to unsubscribe at any time by using the same form. Our list server has been configured in such a way that the e-mail addresses can only be accessed by authorised VAT Calculator staff.
+
+COOKIES
+This Web Site makes use of cookies - a very small text file placed on your hard drive by a web server. Basically, it is your unique identification card that can only be read by the server that gave it to you.
+
+Our server generates one cookie which is used to keep track of the pages you have accessed while using our server. The cookie allows you to page back and forwards through the Web Site and return to pages you have already visited. The cookie exists only for the time you are accessing our server.
+
+Cookies are not used for any other purpose at this Web Site We do not use cookies to collect personal information.
+
+You may prevent the use of cookies by configuring your web browser accordingly. This may, however, hinder the web site's functionality.
+
+INFORMATION LOGGED
+When you visit this Web Site, our server logs the following information provided by your browser:
+
+the type of browser and operating system your computer uses.
+your domain name extension (for example .com, .co.za).
+the referring site's address (the site where you clicked the link that led you to us).
+your server's IP address (a number which is unique to the computer connecting you to the Internet, which is usually one of your service provider's computers).
+the date and time of your visit.
+the address of the pages visited.
+the documents downloaded.
+
+This information is used only for statistical analysis or system administration purposes. It is not associated or correlated with any other information we collect about you.
+
+LINKS TO OTHER SITES
+This Web Site contains links to other sites. VAT Calculator is not responsible for the privacy practices of these web sites.
+
+GOOGLE ANALYTICS
+VAT Calculator has activated the Google Analytics features for Display Advertising including Remarketing, Google Display Network Impression Reporting, and Google Analytics Demographics and Interest Reporting. These features ensure that visitors only receive adverts relevant to them.
+Visitors can opt-out of Google Analytics for Display Advertising and customize Google Display Network ads using the Ads Settings or Google Analytics opt-out browser add-on
+VAT Calculator uses Remarketing with Google Analytics to advertise online.
+Third-party vendors, including Google, show VAT Calculator adverts on sites across the Internet.
+VAT Calculator and third-party vendors, including Google, use first-party cookies (such as the Google Analytics cookie) and third-party cookies (such as the DoubleClick cookie) together to inform, optimize, and serve ads based on a visitor's past visits to websites.
+VAT Calculator and third-party vendors, including Google, use first-party cookies (such as the Google Analytics cookies) and third-party cookies (such as the DoubleClick cookie) together to report how the ad impressions, other uses of ad services, and interactions with these ad impressions and ad services are related to visits to the VAT Calculator site.
+
+GOOGLE DISCLAIMER
+We use third-party advertising companies to serve ads when you visit our website. These companies may use information (not including your name, address, email address, or telephone number) about your visits to this and other website's in order to provide advertisements about goods and services of interest to you. If you would like more information about this practice and to know your choices about not having this information used by these companies, click here.</p>
                 </div>
             </div>
         </div>
